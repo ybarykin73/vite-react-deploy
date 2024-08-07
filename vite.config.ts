a55d83e71ddc49d4bf0d667c58dfdfb9eda5b083
YuraBarykin <yBarykin@gmail.com>
2024-08-07 18:22:25 +0400

    v

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ybarykin73.github.io/vite-react-deploy/"
+  base: "/vite-react-deploy/"
 })
